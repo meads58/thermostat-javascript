e7155016991757b887a5ffa1796621e0eb7c04e1
wardymate <chriswarddev@gmail.com>
2015-03-04 14:12:40 +0000

    refactored tests still all pass

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -5,13 +5,19 @@ describe('Thermostat', function() {
        thermostat = new Thermostat();
     });
 
-  describe('temperature control', function() {
+    function raiseBy(number) {
+      for (var i=0; i <number; i++) {
+        thermostat.up();
+      };
+    };
 
-    function dropEleven() {
-      for (var i=0; i <11; i++) {
+      function dropBy(number) {
+      for (var i=0; i <number; i++) {
         thermostat.down();
       };
     };
+
+  describe('temperature control', function() {
 
     it('should start at 20 degrees', function() {
       expect(thermostat.temperature).toEqual(20);
@@ -28,25 +34,13 @@ describe('Thermostat', function() {
     });
 
     it('should have a minimum temperature of 10', function() {
-      dropEleven();
+      dropBy(11);
       expect(thermostat.temperature).toEqual(10);
     });
 
   });
 
   describe('Power saving mode', function() {
-
-    function raiseSix() {
-      for (var i=0; i <6; i++) {
-        thermostat.up();
-      };
-    };
-
-    function raiseThirteen() {
-      for (var i=0; i <13; i++) {
-        thermostat.up();
-      };
-    };
 
     it('is on by default', function() {
       expect(thermostat.powerSave).toBe(true);
@@ -58,19 +52,19 @@ describe('Thermostat', function() {
     });
 
     it('max temperature is 25 degrees with powersave on', function() {
-      raiseSix();
+      raiseBy(6);
       expect(thermostat.temperature).toEqual(25);
     });
 
     it('max temperature is 32 degrees with powersave off', function() {
       thermostat.saveOff();
-      raiseThirteen();
+      raiseBy(13);
       expect(thermostat.temperature).toEqual(32);
     });
 
     it('change max temperature when switched on', function() {
       thermostat.saveOff();
-      raiseSix();
+      raiseBy(6);
       thermostat.saveOn();
       expect(thermostat.temperature).toEqual(25);
     });
@@ -96,29 +90,17 @@ describe('Thermostat', function() {
 
   describe('colour display', function() {
 
-    function dropThree() {
-      for (var i=0; i <3; i++) {
-        thermostat.down();
-      };
-    };
-
-    function raiseFive() {
-      for (var i=0; i <5; i++) {
-        thermostat.up();
-      };
-    };
-
     it('is yellow when temperature is less than 25', function() {
       expect(thermostat.colour()).toEqual("yellow");
     });
 
     it('is green when less than 18', function() {
-      dropThree();
+      dropBy(3);
       expect(thermostat.colour()).toEqual("green");
     });
 
     it('is red if temperature is 25 or greater', function() {
-      raiseFive();
+      raiseBy(5);
       expect(thermostat.colour()).toEqual("red");
     });
 
