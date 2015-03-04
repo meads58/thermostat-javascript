be6908461dbd716df107f95d4723d485770fc64a
wardymate <chriswarddev@gmail.com>
2015-03-04 11:16:54 +0000

    starts with a temperature of 20. up test fails

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -3,11 +3,18 @@ describe('Thermostat', function() {
 
   describe('temperature control', function() {
 
+    beforeEach(function(){
+       thermostat = new Thermostat();
+    });
+
     it('should start at 20 degrees', function() {
-      thermostat = new Thermostat();
       expect(thermostat.temperature).toEqual(20);
     });
 
+    it('should be able to increase temperature by 1', function() {
+      thermostat.up();
+      expect(thermostat.temperature).toEqual(21);
+    });
   });
 
 });
